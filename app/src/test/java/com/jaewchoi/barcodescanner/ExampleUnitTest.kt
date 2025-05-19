17176250fe8d8f5941fa2b744829aff2032c10f6
Terry Jung <traveler.terry@gmail.com>
2025-05-19 07:50:32 +0200

    test code sample

--- app/src/test/java/com/jaewchoi/barcodescanner/ExampleUnitTest.kt
+++ app/src/test/java/com/jaewchoi/barcodescanner/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.jaewchoi.barcodescanner
 
+import com.jaewchoi.barcodescanner.domain.model.SheetsSettings
+import com.jaewchoi.barcodescanner.utils.SettingValidator
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +15,44 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun `빈 시트 세팅을 넣었을때`() {
+        assertFalse(SettingValidator.checkSheetsSettingValid(
+            SheetsSettings(
+                "",
+                "",
+                "",
+                "",
+                ""
+            )
+        ))
+    }
+
+    @Test
+    fun `fieldCount가 numeric이 아닐때`() {
+        assertFalse(SettingValidator.checkSheetsSettingValid(
+            SheetsSettings(
+                "",
+                "",
+                "",
+                "",
+                ""
+            )
+        ))
+    }
+
+    @Test
+    fun `정확한 SheetSettings값이 들어왔을때`() {
+        assertFalse(SettingValidator.checkSheetsSettingValid(
+            SheetsSettings(
+                "",
+                "",
+                "",
+                "",
+                ""
+            )
+        ))
     }
 }
