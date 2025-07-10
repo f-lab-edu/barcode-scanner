50eaf5a0750e50cbe954b70004ca27332754e3cd
jaewon <suv6629@gmail.com>
2025-07-10 21:30:44 +0900

    Feat: 시트 설정 가이드 페이지 이동 버튼 추가

--- app/src/main/java/com/jaewchoi/barcodescanner/ui/SheetsSettingFragment.kt
+++ app/src/main/java/com/jaewchoi/barcodescanner/ui/SheetsSettingFragment.kt
@@ -1,5 +1,6 @@
 package com.jaewchoi.barcodescanner.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,9 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.jaewchoi.barcodescanner.databinding.FragmentSheetsSettingBinding
-import com.jaewchoi.barcodescanner.domain.model.SheetsSettings
 import com.jaewchoi.barcodescanner.viewmodels.SettingViewModel
+import androidx.core.net.toUri
+import com.jaewchoi.barcodescanner.R
 
 class SheetsSettingFragment : Fragment() {
     private val binding by lazy {
@@ -30,6 +32,12 @@ class SheetsSettingFragment : Fragment() {
             viewModel.saveSheetsSetting { msg ->
                 Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
             }
+        }
+
+        binding.fabHelp.setOnClickListener {
+            val guideUrl = getString(R.string.sheet_setting_guide_url)
+            val intent = Intent(Intent.ACTION_VIEW, guideUrl.toUri())
+            startActivity(intent)
         }
         return binding.root
     }
