5f6766a1a1223fd82553e0d663e51d16827b56b3
jaewon <suv6629@gmail.com>
2025-07-30 20:32:13 +0900

    build: version up 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,9 @@ android {
     defaultConfig {
         applicationId = "com.jaewchoi.barcodescanner"
         minSdk = 27
-        targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        targetSdk = 35
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.jaewchoi.barcodescanner"
